535a955b3a1beec6ad62796b511662700a1d74ff
ggoh29 <56758342+ggoh29@users.noreply.github.com>
2021-01-24 22:01:11 +0800

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -11,9 +11,10 @@ analyser = SentimentIntensityAnalyzer()
 from scipy.special import softmax
 import math
 import copy
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 
 
-source = r"path to directory with csv files"
+source = r"C:/Users/CHRIS/Documents/"
 fake = "Fake.csv"
 real = "True.csv"
 
@@ -56,7 +57,15 @@ class Classifier:
             self.buildMatrix(body_tokens, self.body_dct[type])
             self.buildMemOfTwoMatrix(body_tokens, self.memTwoDct[type])
 
-            
+
+    def smoothAll(self):
+
+        for txt_type in ['real', 'fake']:
+            self.smooth(self.title_dct[txt_type])
+            self.smooth(self.body_dct[txt_type])
+            self.smooth(self.memTwoDct[txt_type])
+
+
     def tokenize(self, text):
         ss = nt.sent_tokenize(text)
         tokenized_sent = [nt.word_tokenize(sent) for sent in ss]
@@ -65,7 +74,7 @@ class Classifier:
         tokens.append(("END", "END"))
         return tokens
 
-    
+
     def buildMatrix(self, tokens_lst, matrix):
 
         for i in range(1, len(tokens_lst)):
@@ -75,7 +84,7 @@ class Classifier:
                 continue
             else:
                 matrix[prev][cur] += 1
-                
+
 
     def buildMemOfTwoMatrix(self, tokens_lst, matrix):
 
@@ -87,14 +96,6 @@ class Classifier:
                 continue
             else:
                 matrix[prevprev + " " + prev][cur] += 1
-                
-              
-    def smoothAll(self):
-
-        for txt_type in ['real', 'fake']:
-            self.smooth(self.title_dct[txt_type])
-            self.smooth(self.body_dct[txt_type])
-            self.smooth(self.memTwoDct[txt_type])
 
 
     def smooth(self, matrix):
@@ -105,37 +106,6 @@ class Classifier:
             for col in matrix[row]:
                 matrix[row][col] = np.log(matrix[row][col]/ acc)
 
-                
-    def generateVector(self, df):
-        vec = []
-
-        cols = df.columns
-        for index, row in df.iterrows():
-            title = row[cols[0]]
-            body = row[cols[1]]
-            vec.append(self.getProbVector(title, body))
-
-        return vec
-
-
-    def getProbVector(self, title, body):
-        title_score = analyser.polarity_scores(title)["compound"]
-        body_score = analyser.polarity_scores(body)["compound"]
-        title, body = self.tokenize(title), self.tokenize(body)
-        title_t_fake_prob = self.getlogProbTransitionProbMemOfOne(title, 'fake', 'title')
-        title_t_real_prob = self.getlogProbTransitionProbMemOfOne(title, 'real', 'title')
-        body_t_fake_prob = self.getlogProbTransitionProbMemOfOne(body, 'fake', 'body')
-        body_t_real_prob = self.getlogProbTransitionProbMemOfOne(body, 'real', 'body')
-        body_t_fake_prob_memTwo = self.getlogProbTransitionProbMemOfTwo(title, 'fake')
-        body_t_real_prob_memTwo = self.getlogProbTransitionProbMemOfTwo(title, 'real')
-        title_prob = softmax([title_t_fake_prob, title_t_real_prob])
-        body_prob = softmax([body_t_fake_prob, body_t_real_prob])
-        body_prob_memTwo = softmax([body_t_fake_prob_memTwo, body_t_real_prob_memTwo])
-        probVector = np.concatenate((title_prob, body_prob, body_prob_memTwo))
-        probVector = list(probVector)
-        probVector.append(title_score)
-        probVector.append(body_score)
-        return probVector
 
     # return logP(x_0 -> x_1 .... | txt_type)
     def getlogProbTransitionProbMemOfOne(self, token_list, txt_type, place):
@@ -172,7 +142,39 @@ class Classifier:
         return acc
 
 
-# The code directly below until the next comment uses https://www.kaggle.com/c/fake-news/data?select=train.csv as dataset
+    def generateVector(self, df):
+        vec = []
+
+        cols = df.columns
+        for index, row in df.iterrows():
+            title = row[cols[0]]
+            body = row[cols[1]]
+            vec.append(self.getProbVector(title, body))
+
+        return vec
+
+
+    def getProbVector(self, title, body):
+        title_score = analyser.polarity_scores(title)["compound"]
+        body_score = analyser.polarity_scores(body)["compound"]
+        title, body = self.tokenize(title), self.tokenize(body)
+        title_t_fake_prob = self.getlogProbTransitionProbMemOfOne(title, 'fake', 'title')
+        title_t_real_prob = self.getlogProbTransitionProbMemOfOne(title, 'real', 'title')
+        body_t_fake_prob = self.getlogProbTransitionProbMemOfOne(body, 'fake', 'body')
+        body_t_real_prob = self.getlogProbTransitionProbMemOfOne(body, 'real', 'body')
+        body_t_fake_prob_memTwo = self.getlogProbTransitionProbMemOfTwo(title, 'fake')
+        body_t_real_prob_memTwo = self.getlogProbTransitionProbMemOfTwo(title, 'real')
+        title_prob = softmax([title_t_fake_prob, title_t_real_prob])
+        body_prob = softmax([body_t_fake_prob, body_t_real_prob])
+        body_prob_memTwo = softmax([body_t_fake_prob_memTwo, body_t_real_prob_memTwo])
+        probVector = np.concatenate((title_prob, body_prob, body_prob_memTwo))
+        probVector = list(probVector)
+        probVector.append(title_score)
+        probVector.append(body_score)
+        return probVector
+
+
+
 alt = "train.csv"
 
 alt = pd.read_csv(source + alt).sort_values(by=['label']).dropna().reset_index()
@@ -185,10 +187,9 @@ real = alt.iloc[number_of_labels[1]:].reset_index().drop(columns = ['index'])
 bucket_train = 700
 bucket_test = 70
 
-# The commented code is uses https://www.kaggle.com/clmentbisaillon/fake-and-real-news-dataset as dataset
 # fake = pd.read_csv(source+fake)
 # real = pd.read_csv(source+real)
-# 
+#
 # bucket_train = 1800
 # bucket_test = 200
 
@@ -212,8 +213,31 @@ for i in range(10):
     for vector in vec:
         x_train.append(vector)
 
-model = GradientBoostingClassifier(learning_rate=0.01, n_estimators=500, max_depth=4, min_samples_split=2,
-                                min_samples_leaf=1, subsample=1,max_features='sqrt', random_state=10)
+
+# hyperparameter tuning - to be commented out once saved
+# p_test = {'learning_rate':[0.01, 0.05, 0.1, 0.25, 0.5], 'n_estimators':[100, 250, 500, 750, 1000]}
+# 
+# tuning = RandomizedSearchCV(estimator=GradientBoostingClassifier(max_depth=4, min_samples_split=2, min_samples_leaf=1, subsample=1,max_features='sqrt', random_state=10),
+#                             param_distributions = p_test, scoring='accuracy', n_jobs=-1, n_iter=10, cv=5)
+# 
+# tuning.fit(np.asarray(x_train), y_train)
+# 
+# lr = tuning.best_params_['learning_rate']
+# ne = tuning.best_params_['n_estimators']
+# 
+# p_test2 = {'max_depth':[2, 3, 4, 5, 6, 7]}
+# 
+# tuning = GridSearchCV(estimator=GradientBoostingClassifier(learning_rate=lr, n_estimators=ne, min_samples_split=2, min_samples_leaf=1, subsample=1,max_features='sqrt', random_state=10),
+#             param_grid = p_test2, scoring='accuracy',n_jobs=4, cv=5)
+# tuning.fit(np.asarray(x_train), y_train)
+# 
+# md = tuning.best_params_['max_depth']
+# 
+# print("params are: learning rate:" + str(lr) + ",n_estimators:" + str(ne) + ",max_depth:" + str(md))
+# "params are: learning rate:0.01,n_estimators:250,max_depth:3"
+
+
+model = GradientBoostingClassifier(learning_rate=0.01, n_estimators=250, max_depth=3, min_samples_split=2, min_samples_leaf=1, subsample=1,max_features='sqrt', random_state=10)
 model.fit(np.asarray(x_train), y_train)
 
 fake_test = fake.tail(bucket_test * 10)
@@ -246,9 +270,9 @@ print(classification_report(y_test, prediction))
 
 #               precision    recall  f1-score   support
 # 
-#            0       0.73      0.83      0.77       700
-#            1       0.80      0.69      0.74       700
+#            0       0.72      0.86      0.79       700
+#            1       0.83      0.67      0.74       700
 # 
-#     accuracy                           0.76      1400
-#    macro avg       0.76      0.76      0.76      1400
-# weighted avg       0.76      0.76      0.76      1400
+#     accuracy                           0.77      1400
+#    macro avg       0.78      0.77      0.76      1400
+# weighted avg       0.78      0.77      0.76      1400
